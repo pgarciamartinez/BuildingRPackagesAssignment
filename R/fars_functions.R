8f5906d00b7a262ff6b84d16c448404e60b2b51e
Pedro <pgarciamartinez@gmail.com>
2020-05-13 08:38:18 +0100

    Updated man file for make_filename.Rd

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -1,21 +1,21 @@
 #' Reading CSV files with "fars_read" function and placing extracted data into data frame.
 #'
-#' This is a simple function that reads a CSV file (after checking if it exists) and places 
-#' its content into a data frame without showing any messages to the user along the process. 
-#' 
+#' This is a simple function that reads a CSV file (after checking if it exists) and places
+#' its content into a data frame without showing any messages to the user along the process.
+#'
 #' @param filename A character string that provides the location and name of the file to import.
-#' 
+#'
 #' @importFrom readr read_csv
-#' 
+#'
 #' @importFrom dplyr tbl_df
-#' 
+#'
 #' @return This function returns a data frame (tbl_df).
-#' 
+#'
 #' @example
 #' fars_read("data/MIE.csv")
 #' sample_data <- fars_read("data/MIE.csv")
-#' 
-#' @export 
+#'
+#' @export
 fars_read <- function(filename) {
         if(!file.exists(filename))
                 stop("file '", filename, "' does not exist")
@@ -25,21 +25,21 @@ fars_read <- function(filename) {
         dplyr::tbl_df(data)
 }
 
-#' Printing "accident_%d.csv.bz2" where %d gets replaced by the year that gets passed as an input.
+#' Printing "accident_d.csv.bz2" where d gets replaced by the year that gets passed as an input.
 #'
 #' This is a simple function that prints out the name of a compressed CSV file depending on the
 #' year that gets passed to the function.
-#' 
+#'
 #' @param year A character string or a numeric that provides the year to be used in the file name.
 #'    The first command within the function coerces the type of the variable year into an integer.
-#' 
-#' @return NULL. This function simply prints out the name of the compressed file that should contain some 
+#'
+#' @return NULL. This function simply prints out the name of the compressed file that should contain some
 #'    data.
-#' 
+#'
 #' @example
 #' make_filename("2003")
 #' make_filename(2010)
-#' 
+#'
 #' @export
 make_filename <- function(year) {
         year <- as.integer(year)
@@ -49,30 +49,30 @@ make_filename <- function(year) {
 #' Showing table with month and year for each year in the list passed as an input.
 #'
 #' In cases where each year in the list is valid, it prints out a table with month and year. In other
-#' cases, where the value provided is not valid, it highlights that it prints out an error message 
+#' cases, where the value provided is not valid, it highlights that it prints out an error message
 #' ("invalid year: " and the troublesome year provided)
-#' 
-#' @param years A list of character strings or numeric values that provides the individual years to 
+#'
+#' @param years A list of character strings or numeric values that provides the individual years to
 #'    be used in the file names
-#'    
+#'
 #' @importFrom dplyr mutate
-#' 
+#'
 #' @importFrom dplyr select
-#' 
-#' @return This function simply prints out tables with month and year for each year contained in the 
+#'
+#' @return This function simply prints out tables with month and year for each year contained in the
 #'    list passed on as an input
-#' 
+#'
 #' @example
 #' fars_read_years(c("2003", "2007", "2012", "2019"))
 #' fars_read_years(c(2003, 2007, 2012, 2019))
-#' 
+#'
 #' @export
 fars_read_years <- function(years) {
         lapply(years, function(year) {
                 file <- make_filename(year)
                 tryCatch({
                         dat <- fars_read(file)
-                        dplyr::mutate(dat, year = year) %>% 
+                        dplyr::mutate(dat, year = year) %>%
                                 dplyr::select(MONTH, year)
                 }, error = function(e) {
                         warning("invalid year: ", year)
@@ -84,60 +84,60 @@ fars_read_years <- function(years) {
 #' Showing table with month and year for each year in the list passed as an input.
 #'
 #' This function first joins the results provided by the function "fars_read_years", then groups the
-#' data by the variables "year" and 'MONTH", followed by the grouping by the number of rows in each 
+#' data by the variables "year" and 'MONTH", followed by the grouping by the number of rows in each
 #' group and finally splitting the year observations into columns.
-#' 
-#' @param years A list of character strings or numeric values that provides the individual years to 
+#'
+#' @param years A list of character strings or numeric values that provides the individual years to
 #'    be used in the file names
-#'    
+#'
 #' @importFrom dplyr bind_rows
-#' 
+#'
 #' @importFrom dplyr group_by
-#' 
+#'
 #' @importFrom dplyr summarize
-#' 
+#'
 #' @importFrom tidyr spread
-#' 
+#'
 #' @return This function simply returns or prints out tables with summaries split by year of the
 #'    the number of observations per year and month
 #'
 #' @example
 #' fars_summarize_years(c("2003", "2007", "2012", "2019"))
 #' fars_summarize_years(c(2003, 2007, 2012, 2019))
-#' 
+#'
 #' @export
 fars_summarize_years <- function(years) {
         dat_list <- fars_read_years(years)
-        dplyr::bind_rows(dat_list) %>% 
-                dplyr::group_by(year, MONTH) %>% 
+        dplyr::bind_rows(dat_list) %>%
+                dplyr::group_by(year, MONTH) %>%
                 dplyr::summarize(n = n()) %>%
                 tidyr::spread(year, n)
 }
 
 #' Shows in a map all the accidents on a given state.
 #'
-#' Before plotting any accident on the map, this function contains two initial checks: first, by 
-#' checking that the state number is valid and, second, by seeing if there were no accidents in that 
+#' Before plotting any accident on the map, this function contains two initial checks: first, by
+#' checking that the state number is valid and, second, by seeing if there were no accidents in that
 #' state. For the first type of check, the error message is "invalid STATE number: %d" with %d being
 #' the state ID provided. The second type of check prints the message "no accidents to plot" if no
 #' observations are present in the data frame for that state.
-#' 
+#'
 #' @param state.num A list of character strings or numeric values that provides the individual state
-#'    
+#'
 #' @param year A character string or a numeric that provides the year to be used in the file.
-#'    
+#'
 #' @importFrom dplyr filter
-#' 
+#'
 #' @importFrom maps map
-#' 
-#' @importFrom graphics points 
-#'    
+#'
+#' @importFrom graphics points
+#'
 #' @return This function plots in a map the locations where accidents occur for a given state.
 #'
 #' @example
 #' fars_map_state("13", "2007")
 #' fars_map_state(15, 2010)
-#' 
+#'
 #' @export
 fars_map_state <- function(state.num, year) {
         filename <- make_filename(year)
