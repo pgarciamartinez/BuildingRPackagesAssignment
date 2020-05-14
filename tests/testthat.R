acb64b0e4d1a246b0fb03e17220b9258e237c5b4
Pedro <pgarciamartinez@gmail.com>
2020-05-14 16:49:20 +0100

    Added 1 simple test

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,4 +2,3 @@ library(testthat)
 library(BuildingRPackagesAssignment)
 
 test_check("BuildingRPackagesAssignment")
-prints_text(fars_summarize_years(c(2013,2014)))
---
+++ tests/testthat/test-test1.R
@@ -0,0 +1,6 @@
+test_that("The function make_filename prints a message", {
+  #expect_that(make_filename(2015), prints_text(make_filename(2015)))
+  expect_error(fars_map_state(1, 40))
+})
+
+
